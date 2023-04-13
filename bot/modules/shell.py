16f35598c698ebc4af9c2d0e55c629c172c27697
oktest145 <83488344+oktest145@users.noreply.github.com>
2023-04-13 09:39:32 +0530

    update bot/modules/shell.py

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -18,6 +18,9 @@ async def shell(client, message):
         await sendMessage(message, 'No command to execute was given.')
         return
       blacklisted_files = ['config.env','token.pickle']
+      for file in blacklisted_files:
+        if file in cmd[1]:
+          await sendMessage(message,f'Usage of {file} is not allowed.' )
      
     cmd = cmd[1]
     stdout, stderr, _ = await cmd_exec(cmd, shell=True)
